99f70f1ebb5cea78c0b0b698094f65b4f8badaaf
Mikołaj Szarapanowski <mszarapanowski@it-dev.pl>
2018-04-24 10:08:30 +0200

    Prepared Webpack

---
+++ main.ts
@@ -0,0 +1,11 @@
+import {MustHaveCoffee} from "./getcoffee";
+import * as $ from "jquery";
+
+class SweetSweetClass {
+    constructor() { 
+        console.log("Even sweeter")
+    }
+}
+
+let basil = new SweetSweetClass()
+let coffee = new MustHaveCoffee()
